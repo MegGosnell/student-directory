6a72fb58636f8a029f5fd221fadf828b712ec9c8
megan gosnell <megan_gosnell@aol.co.uk>
2020-12-14 09:22:52 +0000

    directory.rb file moved to student-directory, list of names added to file, comments added, added array and mathod for refractoring

---
+++ directory.rb
@@ -0,0 +1,31 @@
+#first we print the list of students
+
+students = [
+  "Dr. Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates"
+]
+def print_header
+puts "The students of Villians Academy"
+puts "-------------"
+end
+def print(names)
+names.each do |name|
+  puts name
+end
+end
+def print_footer(names)
+puts "Overall, we have #{students.count} great students"
+end
+
+print_header
+print(students)
+print_footer(students)
